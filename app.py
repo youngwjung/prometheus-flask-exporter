70472a6fba2ec788324d34723cac72a91a88437e
Young Woo Jung <youngwjung@gmail.com>
2022-05-30 16:19:26 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
